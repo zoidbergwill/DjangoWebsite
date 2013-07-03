573c768c608c28b1bd99304a542f8e3ab4bcaa72
William Martin Stewart <zoidbergwill@gmail.com>
2013-07-03 12:53:33 +0200

    Added support for favicon.ico in django 1.5
    Errors in tests, owing to using out dated redirect method in urls.py

--- umonya/umonya/urls.py
+++ umonya/umonya/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 admin.autodiscover()
 
 # Sets custom 404 page
@@ -15,5 +16,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^favicon/.ico$', 'django.views.generic.simple.redirect_to', {'url': 'static/img/pic/favicon.ico'})
+    url(r'^favicon/.ico$', RedirectView.as_view(url='static/img/pic/favicon.ico'))
 )
