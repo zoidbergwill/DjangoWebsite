dc39f765cc65ba1667b206a57e0d27e915634164
William Martin Stewart <zoidbergwill@gmail.com>
2013-07-09 10:17:30 +0200

    Added django debug toolbar
    Changed containers to container fluids, because some pages were changing
    margin a few pixels.
    Added django-debug-toolbar to requirements.txt.
    Fixed indentation in contact.html, and removed blank line in
    registration.html.

--- umonya/settings.py
+++ umonya/settings.py
@@ -105,7 +105,8 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware'
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -138,8 +139,8 @@ INSTALLED_APPS = (
     'apps.main',
     'apps.registration',
     'apps.resources',
+    'debug_toolbar',
     'django_wysiwyg',
-    #~ 'umonya.apps.blog',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
